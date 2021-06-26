4c4d3b16d8d37217ed1e77c87dba4fb3e6a93c2b
Amritanshu Dev Rawat <59793081+Developer-Amritanshu@users.noreply.github.com>
2021-06-26 08:53:59 +0530

    Create Infix_Evaluation.java

---
+++ Stack & Queue/Infix_Evaluation.java
@@ -0,0 +1,77 @@
+import java.util.Stack;
+
+class Infix_Evaluatioin{
+  public static void main(String[] args) {
+    String exp = "(1+2)*3";
+    Stack<Integer> opnds = new Stack<>();
+    Stack<Character> optors = new Stack<>();
+    for(int i =0;i<exp.length();i++){
+      char ch = exp.charAt(i);
+      if(ch=='('){
+        optors.push(ch);
+      }else if(Character.isDigit(ch)){
+        opnds.push(ch-'0');
+      }else if(ch==')'){
+        while(optors.peek()!='('){
+          char optor = optors.pop();
+          int v2 = opnds.pop();
+          int v1 = opnds.pop();
+
+          int opv = operation(v1, v2, optor);
+          opnds.push(opv);
+        }
+        optors.pop();
+      }else if(ch=='+' ||ch=='-' ||ch=='*' ||ch=='/'){
+        while(optors.size()>0 && optors.peek()!='(' 
+        && precedence(ch)<=precedence(optors.peek())){
+          char optor = optors.pop();
+          int v2 = opnds.pop();
+          int v1 = opnds.pop();
+
+          int opv = operation(v1, v2, optor);
+          opnds.push(opv);
+        }
+        optors.push(ch);
+      }
+    }
+    while(optors.size()!=0){
+      char optor = optors.pop();
+      int v2 = opnds.pop();
+      int v1 = opnds.pop();
+
+      int opv = operation(v1, v2, optor);
+      opnds.push(opv);
+    }
+
+    System.out.println(opnds.peek());
+  }
+
+  public static int precedence(char optor){
+    if(optor == '+'){
+     return 1; 
+    }
+    if(optor == '-'){
+     return 1; 
+    }
+    if(optor == '*'){
+     return 2; 
+    }else{
+     return 2; 
+    }
+  }
+
+  public static int operation(int v1,int v2,char optor){
+    if(optor == '+'){
+      return v1+v2; 
+     }
+     if(optor == '-'){
+      return v1-v2; 
+     }
+     if(optor == '*'){
+      return v1*v2; 
+     }else{
+      return v1/v2; 
+     
+  }
+}
+}
