71b1b41595b0aba256789220cd66cbefe86778f3
Amritanshu Dev Rawat <59793081+Developer-Amritanshu@users.noreply.github.com>
2021-06-28 07:06:34 +0530

    Create Postfix_Evaluation.java

---
+++ Stack & Queue/Postfix_Evaluation.java
@@ -0,0 +1,49 @@
+import java.util.Stack;
+
+public class Postfix_Evaluation {
+  public static void main(String[] args) {
+    String eq = "264*8/+3-";
+    Stack<Integer> vs = new Stack<>();
+    Stack<String> is = new Stack<>();
+    Stack<String> ps = new Stack<>();
+
+    for(int i=0;i<eq.length();i++){
+      char ch = eq.charAt(i);
+      if(ch=='+'||ch=='-'||ch=='*'||ch=='/'){
+        int v2 = vs.pop();
+        int v1 = vs.pop();
+        int val = operation(v1,v2,ch);
+        vs.push(val);
+
+        String iv2 = is.pop();
+        String iv1 = is.pop();
+        String ival = "("+iv1+ch+iv2+")";
+        is.push(ival);
+        
+        String pv2 = ps.pop();
+        String pv1 = ps.pop();
+        String pval = ch+pv1+pv2;
+        ps.push(pval);
+      }else{
+        vs.push(ch - '0');
+        is.push(ch+"");
+        ps.push(ch+"");
+      }
+    }
+    System.out.println(vs.pop());
+    System.out.println(is.pop());
+    System.out.println(ps.pop());
+  }
+
+  public static int operation(int v1,int v2, char op){
+    if(op=='+'){
+      return v1+v2;
+    } else if(op=='-'){
+      return v1-v2;
+    }else if(op=='*'){
+      return v1*v2;
+    }else{
+      return v1/v2;
+    }
+  }
+}
